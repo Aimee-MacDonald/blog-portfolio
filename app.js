178c15c592f77f2d904fd0661c664b9ae4a37477
Aimée <admin@aimeemacdonald.com>
2018-01-19 17:06:44 +0000

    no js endpoints

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ app.set("view engine", "pug");
 app.use(express.static(__dirname + "/static"));
 
 app.get("/", (req, res) => res.render("index"));
+
+app.get("/portfolio", (req, res) => res.render("portfolio"));
+app.get("/blog", (req, res) => res.render("blog"));
 app.listen(process.env.PORT || 8080);
--- static/index.js
+++ static/index.js
@@ -1,3 +1,4 @@
+
 document.querySelector("#blog").href = "#0";
 document.querySelector("#portfolio").href = "#1";
 
@@ -6,22 +7,22 @@ checkHash();
 function checkHash(){
   if(!location.hash){
     // Show the Home Page
-    document.querySelector("#blog").className = "homeNav";
-    document.querySelector("#portfolio").className = "homeNav";
+    document.querySelector("#blog").className = "hnOption";
+    document.querySelector("#portfolio").className = "hnOption";
     console.log("Home");
   } else {
     // Check for "Blog" or "Portfolio"
     switch(location.hash[1]){
       case "0": // Blog
-        document.querySelector("#blog").className = "homeNav selected";
-        document.querySelector("#portfolio").className = "homeNav unselected";
+        document.querySelector("#blog").className = "hnOption selected";
+        document.querySelector("#portfolio").className = "hnOption unselected";
         console.log("Blog");
         break;
 
       case "1": // Portfolio
         console.log("Portfolio");
-        document.querySelector("#blog").className = "homeNav unselected";
-        document.querySelector("#portfolio").className = "homeNav selected";
+        document.querySelector("#blog").className = "hnOption unselected";
+        document.querySelector("#portfolio").className = "hnOption selected";
         break
 
       default: // No such URL
